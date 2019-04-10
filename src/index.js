b7ee1bb014121d963e6bb72718945d4a7c37d622
dlousa92 <dlousa92@gmail.com>
2019-04-10 11:04:49 -0400

    create Taiwan Fdi's  line chart

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import chartVessels from './js/charts/chartVessels'
+import taiwanFdis from './js/charts/taiwanFdis'
 import theme from './js/theme'
 
 window.addEventListener('DOMContentLoaded', () => {
   Highcharts.setOptions(theme)
   chartVessels()
+  taiwanFdis()
 })
