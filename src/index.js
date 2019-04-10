478c813ae224a8a678f3e134a539ffa15933ff0f
dlousa92 <dlousa92@gmail.com>
2019-04-10 11:42:14 -0400

    import chart & display

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import chartVessels from './js/charts/chartVessels'
 import fdiToAsean from './js/charts/fdiToAsean'
+import fdiToSoutheast from './js/charts/fdiToSoutheast'
 import theme from './js/theme'
 
 window.addEventListener('DOMContentLoaded', () => {
   Highcharts.setOptions(theme)
   chartVessels()
   fdiToAsean()
+  fdiToSoutheast()
 })
