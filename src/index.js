875e76a4a9a7dc32521df724f96fd9a17cacb563
dlousa92 <dlousa92@gmail.com>
2019-04-10 12:57:25 -0400

    create and import per capita growth chart

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import chartVessels from './js/charts/chartVessels'
 import fdiToAsean from './js/charts/fdiToAsean'
 import fdiToSoutheast from './js/charts/fdiToSoutheast'
+import perCapitaGrowth from './js/charts/perCapitaGrowth'
 import theme from './js/theme'
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -8,4 +9,5 @@ window.addEventListener('DOMContentLoaded', () => {
   chartVessels()
   fdiToAsean()
   fdiToSoutheast()
+  perCapitaGrowth()
 })
---
+++ src/js/charts/perCapitaGrowth.js
@@ -0,0 +1,81 @@
+import Highcharts from 'Highcharts'
+console.log(window)
+
+const perCapitaGrowth = () => {
+  Highcharts.chart('percapitagrowth', {
+    // Load Data in from Google Sheets
+    data: {
+      googleSpreadsheetKey: '1ovEaZyhAmti9u9Db1arWwm14mkLxH-fZhO_ccnzMPrs',
+      googleSpreadsheetWorksheet: 1
+    },
+    // General Chart Options
+    chart: {
+      type: 'line'
+    },
+    // Colors
+    // colors: Highcharts.getOptions().colors,
+    // Chart Title and Subtitle
+    title: {
+      text: 'GDP per capita growth of selected countries, 1995-2000'
+    },
+    // subtitle: {
+    //   text: 'Optional Subtitle'
+    // },
+    // Credits
+    credits: {
+      enabled: true,
+      href: false,
+      text: 'Source: GDP per capita growth of selected countries, 1995-2000'
+    },
+    // Chart Legend
+    legend: {
+      //   title: {
+      //     text:
+      //       'Legend Title<br/><span style="font-size: 12px; color: #808080; font-weight: normal">(Click to hide)</span>'
+      //   },
+      align: 'center',
+      verticalAlign: 'bottom',
+      layout: 'horizontal'
+    },
+    // X Axis
+    xAxis: {
+      allowDecimals: false,
+      pane: {
+        center: ['100%', '100%']
+      }
+    },
+    // Y Axis
+    yAxis: {
+      title: {
+        text: ''
+      },
+      allowDecimals: false,
+      tickInterval: 4
+      // labels: {
+      //   format: "${value}"
+      // }
+    },
+    // Tooltip
+    tooltip: {
+      headerFormat:
+        '<span style="font-size: 13px;text-align:center;margin-bottom: 5px;font-weight: bold;font-family: \'Roboto\', arial, sans-serif;">{point.key}</span><br/>',
+      pointFormatter: function() {
+        return `<span style="color:${this.color}">\u25CF </span>
+        ${this.y}`
+      }
+    },
+    // Additional Plot Options
+    plotOptions: {
+      line: {
+        marker: {
+          enabled: false,
+          symbol: 'circle',
+          radius: 3
+        },
+        lineWidth: 3
+      }
+    }
+  })
+}
+
+export default perCapitaGrowth
