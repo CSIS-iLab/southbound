c8842f1864844219b11a28389edf8d6ee5b429ed
dlousa92 <dlousa92@gmail.com>
2019-04-10 15:53:35 -0400

    create outbound investment in NSP countries chart

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import fdiToSoutheast from './js/charts/fdiToSoutheast'
 import perCapitaGrowth from './js/charts/perCapitaGrowth'
 import unemployment from './js/charts/unemployment'
 import exportToNSP from './js/charts/exportToNSP'
+import outboundInvestment from './js/charts/outboundInvestment'
 import theme from './js/theme'
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -14,4 +15,5 @@ window.addEventListener('DOMContentLoaded', () => {
   perCapitaGrowth()
   unemployment()
   exportToNSP()
+  outboundInvestment()
 })
