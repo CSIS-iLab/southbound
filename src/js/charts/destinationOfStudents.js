c216809aab3b865b830a7f571cbc34ec6aa1f321
dlousa92 <dlousa92@gmail.com>
2019-04-17 09:26:35 -0400

    reformat for prettier

--- src/js/charts/destinationOfStudents.js
+++ src/js/charts/destinationOfStudents.js
@@ -14,12 +14,14 @@ const destinationOfStudents = () => {
     // colors: Highcharts.getOptions().colors,
     // Chart Title and Subtitle
     title: {
-      text: 'Study Abroad Destination of Outbound Students From Taiwan, 2015-2016'
+      text:
+        'Study Abroad Destination of Outbound Students From Taiwan, 2015-2016'
     },
     // Credits
     credits: {
       enabled: true,
-      href: 'https://depart.moe.edu.tw/ed2500/News_Content.aspx?n=2D25F01E87D6EE17&sms=4061A6357922F45A&s=ECAAD5FFD81C8057',
+      href:
+        'https://depart.moe.edu.tw/ed2500/News_Content.aspx?n=2D25F01E87D6EE17&sms=4061A6357922F45A&s=ECAAD5FFD81C8057',
       text:
         'CSIS China Power Project | Source: Statistical Table on Taiwanese Students Studying Abroad in Major Countries Around the World for 2016, Ministry of Education, ROC (Taiwan), December 22, 2016'
     },
@@ -43,9 +45,9 @@ const destinationOfStudents = () => {
     tooltip: {
       headerFormat:
         '<span style="font-size: 13px;text-align:center;margin-bottom: 5px;font-weight: bold;font-family: \'Roboto\', arial, sans-serif;">{point.key}</span><br/>',
-      pointFormatter: function () {
+      pointFormatter: function() {
         return `<span style="color:${this.color}">\u25CF </span>
-               $${this.y.toFixed(2)}%`
+               ${this.y.toFixed(2)}%`
       }
     },
     // Additional Plot Options
@@ -63,10 +65,12 @@ const destinationOfStudents = () => {
         size: '90%'
       }
     },
-    series: [{
-      index: 0,
-      innerSize: '70%'
-    }],
+    series: [
+      {
+        index: 0,
+        innerSize: '70%'
+      }
+    ],
     responsive: {
       rules: [
         {
