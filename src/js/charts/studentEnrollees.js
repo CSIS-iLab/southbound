6b8b1d871bce43e234fc7e91e5821aec7fb473a1
dlousa92 <dlousa92@gmail.com>
2019-04-19 11:25:48 -0400

    categorize data into other and then display on toolbar

--- src/js/charts/studentEnrollees.js
+++ src/js/charts/studentEnrollees.js
@@ -5,7 +5,23 @@ const studentEnrollees = () => {
     // Load Data in from Google Sheets
     data: {
       googleSpreadsheetKey: '1zl-WEfAt1KBexvX_NsC-2-K0aSMHdVYUk1tgnYLTBtQ',
-      googleSpreadsheetWorksheet: 1
+      googleSpreadsheetWorksheet: 1,
+      complete: function (data) {
+        // Filter out every piece of data that isn't considered a part of Other
+        const filteredData = data.series.filter(item => item.name !== 'Indonesia' && item.name !== 'Vietnam' && item.name !== 'Mainland China' && item.name !== 'Other')
+
+        // Set these series to invisible on the chart so they don't appear
+        filteredData.forEach(item => {
+          item.visible = false
+        })
+
+        // pass these items into the 'Other' series of data as an object key that we can then access later in the toolbar using 'this'
+        data.series.forEach(item => {
+          if (item.name === 'Other') {
+            item.subData = filteredData
+          }
+        })
+      }
     },
     // General Chart Options
     chart: {
@@ -48,11 +64,34 @@ const studentEnrollees = () => {
     tooltip: {
       headerFormat:
         '<span style="font-size: 13px;text-align:center;margin-bottom: 5px;font-weight: bold;font-family: \'Roboto\', arial, sans-serif;">{point.key}</span><br/>',
-      pointFormatter: function() {
-        return `<span style="color:${this.color}">\u25CF </span>
-                ${this.series.name}<br>
-               $${this.y.toString().substring(0, 2)} Thousand`
-      }
+
+        pointFormatter: function() {
+          const dataSet = this.series
+          let toolbarData = []
+  
+          if ( dataSet.name === 'Other' ) {
+            dataSet.userOptions.subData.forEach(DataItem => {
+              for (let i = 0; i < DataItem.data.length; i++) {
+                if (DataItem.data[i][0] === this.category ) {
+                  const dataForYear = {
+                    name: DataItem.name,
+                    data: DataItem.data[i][1]
+                  }
+                  toolbarData.push(dataForYear)
+                }
+              }
+            })
+            
+            const toolbarFormat = `${toolbarData.map(dataItem => {
+              return `<br><span style="color:${this.color}"> \u25CF </span>${dataItem.name}: $${dataItem.data}`
+            })}`
+            return toolbarFormat
+          } else {
+          return `<span style="color:${this.color}">\u25CF </span>
+                  ${this.series.name}<br>
+                 $${this.y.toString()}`
+          }
+        }
     },
     // Additional Plot Options
     plotOptions: {
