daed629a2abf08e8394e8dceb939377361dcc257
leezaweb <leeza.web@gmail.com>
2019-05-17 16:08:00 -0400

    fix loader

--- config-overrides.js
+++ config-overrides.js
@@ -1,44 +1,39 @@
-const StyleLintPlugin = require("stylelint-webpack-plugin");
+const StyleLintPlugin = require('stylelint-webpack-plugin')
 
 module.exports = function override(config, env) {
   config.externals = {
-    SmoothScroll: "smooth-scroll",
-    Highcharts: "Highcharts"
-  };
-  config.devtool = "inline-source-map";
+    SmoothScroll: 'smooth-scroll',
+    Highcharts: 'Highcharts'
+  }
+  config.devtool = 'inline-source-map'
 
   config.module.rules.push({
-    test: /\.(css)$/,
-    use: [{ loader: "css-loader", options: { sourceMap: false } }]
-  });
-
-  config.module.rules.push({
-    test: /\.(scss)$/,
-    use: [{ loader: "postcss-loader" }, { loader: "sass-loader" }]
-  });
+    test: /\.(scss|css)$/,
+    use: [{ loader: 'postcss-loader' }, { loader: 'sass-loader' }]
+  })
 
   config.module.rules.push({
     test: /\.(config)$/,
-    loader: "file-loader?name=[name].[ext]"
-  });
+    loader: 'file-loader?name=[name].[ext]'
+  })
 
   config.module.rules.push({
     test: /\.(js|jsx)$/,
     exclude: /node_modules/,
-    loader: "babel-loader",
+    loader: 'babel-loader',
 
     options: {
       presets: [
-        "@babel/preset-env",
-        "@babel/preset-react",
+        '@babel/preset-env',
+        '@babel/preset-react',
         {
-          plugins: ["@babel/plugin-proposal-class-properties"]
+          plugins: ['@babel/plugin-proposal-class-properties']
         }
       ]
     }
-  });
+  })
 
-  config.plugins.push(new StyleLintPlugin({ fix: true }));
+  config.plugins.push(new StyleLintPlugin({ fix: true }))
 
-  return config;
-};
+  return config
+}
