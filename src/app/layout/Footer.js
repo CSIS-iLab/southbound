5876c6d2bedaecce5a54975e1e161db39a42e7b6
dlousa92 <dlousa92@gmail.com>
2019-06-14 11:49:22 -0400

    add content to footer

--- src/app/layout/Footer.js
+++ src/app/layout/Footer.js
@@ -1,13 +1,98 @@
 import React from 'react'
-import logo from '../../assets/images/csis_logo-short.svg'
+import GetData from '../helpers/GetData'
+import ValueToJSX from '../helpers/ValueToJSX'
+import logo from '../../assets/images/csis_logo-long.svg'
 
 export default class Footer extends React.Component {
   render() {
+    const FooterContent = GetData('footer')
+    const { siteStructure } = this.props
+
     return (
-      <footer>
-        <img src={logo} className="Page-logo" alt="logo" />
-        Footer Footer Footer Footer Footer Footer Footer Footer Footer Footer
-        Footer Footer
+      <footer className="site-footer">
+        <section className="site-footer__description">
+          <a
+            href="https://www.csis.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="csis-link"
+          >
+            <img 
+              src={logo} 
+              className="csis-logo"  
+              alt="Center for Strategic and International Studies"
+              title="Center for Strategic and   International Studies" 
+            />
+          </a>
+
+          {ValueToJSX(FooterContent.description[0])}
+
+          <ul className="site-footer__description-social">Follow Us
+            <li>
+              <a
+                href="https://www.facebook.com/CSIS.org/"
+                className="icon icon-facebook"
+              >
+                <span className="visually-hidden">Facebook</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/CSIS" className="icon icon-twitter">
+                <span className="visually-hidden">Twitter</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/company/csis"
+                className="icon icon-linkedin"
+              >
+                <span className="visually-hidden">LinkedIn</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.youtube.com/channel/UCr5jq6MC_VCe1c5ciIZtk_w"
+                className="icon icon-youtube"
+              >
+                <span className="visually-hidden">Youtube</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/csis/?hl=en"
+                className="icon icon-instagram"
+              >
+                <span className="visually-hidden">Instagram</span>
+              </a>
+            </li>
+          </ul>
+        </section>
+
+        <section className="site-footer__copyright">
+          {ValueToJSX(FooterContent.copyright[0])}
+        </section>
+
+        <section className="site-footer__report">
+          <div>
+            {Object.keys(FooterContent.southbound.report).map(key => {
+              return ValueToJSX(FooterContent.southbound.report[key], null, key)
+            })}
+          </div>
+
+          <div>
+            {Object.keys(FooterContent.southbound.authors).map(key => {
+              return ValueToJSX(FooterContent.southbound.authors[key], null, key)
+            })}
+          </div>
+          <div>
+            {Object.keys(FooterContent.southbound.info).map(key => {
+              return ValueToJSX(FooterContent.southbound.info[key], null, key)
+            })}
+          </div>
+          <div>
+            {ValueToJSX(FooterContent.southbound.download)}
+          </div>
+        </section>
       </footer>
     )
   }
--- src/content/footer.md
+++ src/content/footer.md
@@ -1,23 +1,24 @@
 ---
 slug: footer
 description:
-  - |-
+  - |- 
     In hac habitasse platea dictumst. Vivamus adipiscing fermentum quam volutpat aliquam. Integer et elit eget elit facilisis tristique. Nam vel iaculis mauris. Sed ullamcorper tellus erat, non ultrices sem tincidunt euismod. Fusce rhoncus porttitor velit, eu bibendum nibh aliquet vel.
-copywrite:
+copyright:
   - |-
-    <p class="copywrite">©2019 by the Center for Strategic <br>
+    <p class="copyright">©2019 by the Center for Strategic <br>
       and  International Studies. All rights reserved.<br><a class="privacy-policy" href="https://www.csis.org/privacy-policy" target="_blank" rel="nofollow">Privacy Policy</a> | <a class="privacy-policy" href="https://www.csis.org/reprint-permissions" target="_blank" rel="nofollow">Reprint Permissions</a></p>
 southbound:
-  title: The New Southbound Policy:
-  subtitle: Deepening Taiwan’s Regional Integration
-  by: by
-  by_authors: Bonnie S. Glaser, Scott Kennedy, Derek Mitchell
-  with: with
-  with_authors: Matthew P. Funaiole
-  ISBN: ISBN#
-  ISBN_info: 978-1-4422-8053-3 (pb); 978-1-4422-8054-0 (eBook)
-  publisher: CSIS/Rowman & Littlefield
+  report:
+    title: <h4>The New Southbound Policy</h4>
+    subtitle: Deepening Taiwan’s Regional Integration
+  authors:
+    by: by
+    by_authors: Bonnie S. Glaser, Scott Kennedy, Derek Mitchell
+    with_authors: with Matthew P. Funaiole
+  info:
+    ISBN: ISBN#
+    ISBN_info: 978-1-4422-8053-3 (pb); 978-1-4422-8054-0 (eBook)
+    publisher: CSIS/Rowman & Littlefield
   download: |-
-      <a href="#" class="link-download">Download the Full Report</a>
-
+      <a href="#" class="download">Download the Full Report</a>
 ---
