3a6d07f8673ef543edbf58c17235e727aaa1d435
leezaweb <leeza.web@gmail.com>
2019-06-17 17:04:44 -0400

    fix footer

--- src/app/layout/Footer.js
+++ src/app/layout/Footer.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import GetData from '../helpers/GetData'
 import ValueToJSX from '../helpers/ValueToJSX'
-import logo from '../../assets/images/csis_logo-long.svg'
-import southboundReport from '../../assets/images/southbound-cover.png'
+import { ReactComponent as Logo } from '../../assets/images/csis_logo-long.svg'
 
 export default class Footer extends React.Component {
   render() {
@@ -11,7 +10,7 @@ export default class Footer extends React.Component {
 
     return (
       <footer className="site-footer">
-        <div className="breakpoint">
+        <div className="wrapper">
           <section className="site-footer__description">
             <a
               href="https://www.csis.org/"
@@ -19,20 +18,17 @@ export default class Footer extends React.Component {
               rel="noopener noreferrer"
               className="csis-link"
             >
-              <img 
-                src={logo} 
-                className="csis-logo"  
+              <Logo
+                className="csis-link__logo"
                 alt="Center for Strategic and International Studies"
-                title="Center for Strategic and   International Studies" 
+                title="Center for Strategic and   International Studies"
               />
             </a>
-          
             <div className="site-footer__description-details">
               {ValueToJSX(FooterContent.description[0])}
             </div>
-
             <div className="site-footer__description-social">
-              <h5 className="faded-half">Follow Us</h5>
+              {ValueToJSX(FooterContent.follow[0])}
               <ul className="share">
                 <li>
                   <a
@@ -43,7 +39,10 @@ export default class Footer extends React.Component {
                   </a>
                 </li>
                 <li>
-                  <a href="https://twitter.com/CSIS" className="icon icon-twitter">
+                  <a
+                    href="https://twitter.com/CSIS"
+                    className="icon icon-twitter"
+                  >
                     <span className="visually-hidden">Twitter</span>
                   </a>
                 </li>
@@ -72,51 +71,49 @@ export default class Footer extends React.Component {
                   </a>
                 </li>
               </ul>
-            </div>
+            </div>{' '}
+            <section className="site-footer__description-copyright ">
+              {ValueToJSX(FooterContent.copyright[0])}
+            </section>
           </section>
 
-          <section className="site-footer__description--copyright faded-half">
-            {ValueToJSX(FooterContent.copyright[0])}
+          <section className="site-footer__report">
+            <div className="site-footer__report-image">
+              <a
+                href="https://www.csis.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={FooterContent.southbound.cover}
+                  alt="The New Southbound Policy Report Cover"
+                  title="Download The New Southbound Policy Report"
+                />
+              </a>
+            </div>
+            <div className="site-footer__report-details">
+              <div className="site-footer__report-details-title">
+                {Object.keys(FooterContent.southbound.report).map(key => {
+                  return ValueToJSX(FooterContent.southbound.report[key], key)
+                })}
+              </div>
+
+              <div className="site-footer__report-details-authors">
+                {Object.keys(FooterContent.southbound.authors).map(key => {
+                  return ValueToJSX(FooterContent.southbound.authors[key], key)
+                })}
+              </div>
+              <div className="site-footer__report-details-info">
+                {Object.keys(FooterContent.southbound.info).map(key => {
+                  return ValueToJSX(FooterContent.southbound.info[key], key)
+                })}
+              </div>
+              {ValueToJSX(FooterContent.southbound.download)}
+            </div>
           </section>
         </div>
-
-        <section className="site-footer__report">
-          <div className="site-footer__report-image">
-            <a
-              href="https://www.csis.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="csis-link"
-            >
-              <img 
-                src={southboundReport} 
-                className="csis-logo"  
-                alt="Center for Strategic and International Studies"
-                title="Center for Strategic and   International Studies" 
-              />
-            </a>
-          </div>
-          <div className="site-footer__report-details">
-            <div className='site-footer__report-details-title'>
-              {Object.keys(FooterContent.southbound.report).map(key => {
-                return ValueToJSX(FooterContent.southbound.report[key], null, key)
-              })}
-            </div>
-
-            <div className="site-footer__report-details-authors">
-              {Object.keys(FooterContent.southbound.authors).map(key => {
-                return ValueToJSX(FooterContent.southbound.authors[key],null, key)
-              })}
-            </div>
-            <div className="site-footer__report-details-info">
-              {Object.keys(FooterContent.southbound.info).map(key => {
-                return ValueToJSX(FooterContent.southbound.info[key],   null, key)
-              })}
-            </div>
-            <div className='site-footer__report-details-download'>
-              <li>{ValueToJSX(FooterContent.southbound.download)}</li>
-            </div>
-          </div>
+        <section className="site-footer__copyright">
+          {ValueToJSX(FooterContent.copyright[0])}
         </section>
       </footer>
     )
--- src/content/footer.md
+++ src/content/footer.md
@@ -1,24 +1,36 @@
 ---
 slug: footer
+
 description:
-  - |- 
+  - |-
     In hac habitasse platea dictumst. Vivamus adipiscing fermentum quam volutpat aliquam. Integer et elit eget elit facilisis tristique. Nam vel iaculis mauris. Sed ullamcorper tellus erat, non ultrices sem tincidunt euismod. Fusce rhoncus porttitor velit, eu bibendum nibh aliquet vel.
+follow:
+  - |-
+      <div>Follow Us</div>
 copyright:
   - |-
-    <p class="copyright">©2019 by the Center for Strategic
-      and  International Studies. All rights reserved. |<a class="privacy-policy" href="https://www.csis.org/privacy-policy" target="_blank" rel="nofollow"> Privacy Policy</a> | <a class="privacy-policy" href="https://www.csis.org/reprint-permissions" target="_blank" rel="nofollow">Reprint Permissions</a></p>
+    ©2019 by the Center for Strategic
+      and  International Studies. All rights reserved. |<a class="privacy-policy" href="https://www.csis.org/privacy-policy" target="_blank" rel="nofollow"> Privacy Policy</a> | <a class="privacy-policy" href="https://www.csis.org/reprint-permissions" target="_blank" rel="nofollow">Reprint Permissions</a>
 southbound:
+  cover: 'https://res.cloudinary.com/csisideaslab/image/upload/v1560804941/southbound/southbound-cover.png'
   report:
-    title: <h4>The New Southbound Policy</h4>
+    title: |-
+           #### The New Southbound Policy
     subtitle: Deepening Taiwan’s Regional Integration
   authors:
-    by: by
-    by_authors: Bonnie S. Glaser, Scott Kennedy, Derek Mitchell
-    with_authors: with Matthew P. Funaiole
+    by: |-
+           <div>by</div>
+    by_authors: |-
+           <div>Bonnie S. Glaser, Scott Kennedy, Derek Mitchell</div>
+    with_authors: |-
+           <div>with Matthew P. Funaiole</div>
   info:
-    ISBN_info: ISBN# 978-1-4422-8053-3 
-    ISBN_addtnl: (pb); 978-1-4422-8054-0 (eBook)
-    publisher: CSIS/Rowman & Littlefield
+    ISBN_paperback: |-
+           <div>ISBN# 978-1-4422-8053-3  (pb)
+    ISBN_ebook: |-
+           <div>978-1-4422-8054-0 (eBook)
+    publisher: |-
+           <div>CSIS/Rowman & Littlefield
   download: |-
-      <a href="#" class="icon-download">Download the Full Report</a>
+        <a href="@" className="download"><img src="https://res.cloudinary.com/csisideaslab/image/upload/v1560804941/southbound/southbound-cover.png" class="download__image" alt="The New Southbound Policy Report Cover" title="Download The New Southbound Policy Report"><span  class="icon-download">Download the Full Report</span></a>
 ---
